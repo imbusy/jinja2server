de92835fd1f0d199709ba5a5173a2a5e46fb1f66
Lukas Steiblys <imbusy@imbusy.org>
2016-02-04 19:09:55 -0800

    ability to list directories and use GET parameters

--- src/application.py
+++ src/application.py
@@ -1,17 +1,25 @@
 from collections import namedtuple
 from wsgiref.util import request_uri
-from urllib.parse import urlparse
+from urllib.parse import urlparse, parse_qs
 from jinja2 import Environment, FileSystemLoader, TemplateNotFound
+import json
+import os
 
 templates = Environment(loader=FileSystemLoader('/home/web/templates'), auto_reload=True)
 
 def application(env, start_response):
   URI = request_uri(env)
+  get_parameters = parse_qs(env['QUERY_STRING'])
   parsed = urlparse(URI)
   path = parsed.path
   if path == '/':
     path = 'index.html'
   path = path.replace('..', '')
+
+  if os.path.exists('/home/web/templates'+path):
+    # this is a directory name - return files as json array
+    start_response('200 OK', [('Content-Type','application/json')])  
+    return [json.dumps(sorted(os.listdir('/home/web/templates'+path))).encode('utf-8')]
 
   FileContent = namedtuple('FileContent', ['content_type', 'binary'])
   # content-type, binary
@@ -29,7 +37,7 @@ def application(env, start_response):
   file_content = extensions.get(path.split('.')[-1], FileContent('application/octet-stream', True))
   try:
     if not file_content.binary:
-      result = templates.get_template(path).render().encode('utf-8')
+      result = templates.get_template(path).render(GET=get_parameters).encode('utf-8')
     else:
       result = open('/home/web/templates/'+path,'rb').read()
   except TemplateNotFound:
